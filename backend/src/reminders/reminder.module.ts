7d98e3478fa48f83f23d6ee12fad95e51b5105f5
Nanjar Budiman <nanjar.budiman@gmail.com>
2026-08-21 20:01:28 +0700

    feat(reminders): connect notification provider

--- backend/src/reminders/reminder.module.ts
+++ backend/src/reminders/reminder.module.ts
@@ -1,8 +1,2 @@
-import { Module } from '@nestjs/common';
-import { ScheduleModule } from '@nestjs/schedule';
-import { PrismaModule } from '../prisma/prisma.module';
-import { RemindersController } from './reminders.controller';
-import { ReminderService } from './reminder.service';
-
-@Module({ imports: [ScheduleModule.forRoot(), PrismaModule], controllers: [RemindersController], providers: [ReminderService], exports: [ReminderService] })
-export class ReminderModule {}
+import { Module } from '@nestjs/common'; import { ScheduleModule } from '@nestjs/schedule'; import { PrismaModule } from '../prisma/prisma.module'; import { NotificationModule } from '../notifications/notification.module'; import { RemindersController } from './reminders.controller'; import { ReminderService } from './reminder.service';
+@Module({ imports:[ScheduleModule.forRoot(),PrismaModule,NotificationModule], controllers:[RemindersController], providers:[ReminderService], exports:[ReminderService] }) export class ReminderModule {}
