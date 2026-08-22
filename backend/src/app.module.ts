0eff0b83043904134ead0b4fab52292d148442c5
Nanjar Budiman <nanjar.budiman@gmail.com>
2026-08-22 14:19:49 +0700

    feat: enable reminder scheduler

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,16 +1,22 @@
 import { Module } from '@nestjs/common';
+import { ScheduleModule } from '@nestjs/schedule';
 import { AuthModule } from './auth/auth.module';
 import { DashboardModule } from './dashboard/dashboard.module';
 import { DocumentsModule } from './documents/documents.module';
+import { NotificationsModule } from './notifications/notifications.module';
 import { PrismaModule } from './prisma/prisma.module';
+import { SchedulerModule } from './scheduler/scheduler.module';
 import { UsersModule } from './users/users.module';
 
 @Module({
   imports: [
+    ScheduleModule.forRoot(),
     PrismaModule,
     AuthModule,
     DashboardModule,
     DocumentsModule,
+    NotificationsModule,
+    SchedulerModule,
     UsersModule,
   ],
 })
