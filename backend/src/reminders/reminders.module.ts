170a642fda6a3c6380de3596b7dde2c1c8d5f417
Nanjar Budiman <nanjar.budiman@gmail.com>
2026-08-22 18:34:06 +0700

    feat: expose reminder workspace controller

--- backend/src/reminders/reminders.module.ts
+++ backend/src/reminders/reminders.module.ts
@@ -2,11 +2,12 @@ import { Module } from '@nestjs/common';
 import { AuditModule } from '../audit/audit.module';
 import { AuthModule } from '../auth/auth.module';
 import { RemindersController } from './reminders.controller';
+import { RemindersOverviewController } from './reminders-overview.controller';
 import { RemindersService } from './reminders.service';
 
 @Module({
   imports: [AuthModule, AuditModule],
-  controllers: [RemindersController],
+  controllers: [RemindersController, RemindersOverviewController],
   providers: [RemindersService],
   exports: [RemindersService],
 })
