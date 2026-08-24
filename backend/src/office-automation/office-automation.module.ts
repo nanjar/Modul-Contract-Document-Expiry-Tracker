d3fabc45763d4316c099ca922165e351e2ed8d9c
Nanjar Budiman <nanjar.budiman@gmail.com>
2026-08-24 15:25:34 +0700

    feat: register office automation query service

--- backend/src/office-automation/office-automation.module.ts
+++ backend/src/office-automation/office-automation.module.ts
@@ -4,11 +4,12 @@ import { PrismaModule } from '../prisma/prisma.module';
 import { UsersModule } from '../users/users.module';
 import { OfficeAutomationController } from './office-automation.controller';
 import { OfficeAutomationService } from './office-automation.service';
+import { OfficeAutomationQueryService } from './office-automation-query.service';
 
 @Module({
   imports: [AuthModule, PrismaModule, UsersModule],
   controllers: [OfficeAutomationController],
-  providers: [OfficeAutomationService],
-  exports: [OfficeAutomationService],
+  providers: [OfficeAutomationService, OfficeAutomationQueryService],
+  exports: [OfficeAutomationService, OfficeAutomationQueryService],
 })
 export class OfficeAutomationModule {}
