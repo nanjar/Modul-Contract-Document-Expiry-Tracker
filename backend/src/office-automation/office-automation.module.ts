2d3ba0562803b20055f979814e9e784718d117f9
Nanjar Budiman <nanjar.budiman@gmail.com>
2026-08-27 16:16:16 +0700

    feat(office): register attachment service and storage

--- backend/src/office-automation/office-automation.module.ts
+++ backend/src/office-automation/office-automation.module.ts
@@ -1,15 +1,17 @@
 import { Module } from '@nestjs/common';
 import { AuthModule } from '../auth/auth.module';
 import { PrismaModule } from '../prisma/prisma.module';
+import { StorageModule } from '../storage/storage.module';
 import { UsersModule } from '../users/users.module';
 import { OfficeAutomationController } from './office-automation.controller';
 import { OfficeAutomationService } from './office-automation.service';
 import { OfficeAutomationQueryService } from './office-automation-query.service';
+import { OfficeAttachmentService } from './office-attachment.service';
 
 @Module({
-  imports: [AuthModule, PrismaModule, UsersModule],
+  imports: [AuthModule, PrismaModule, StorageModule, UsersModule],
   controllers: [OfficeAutomationController],
-  providers: [OfficeAutomationService, OfficeAutomationQueryService],
-  exports: [OfficeAutomationService, OfficeAutomationQueryService],
+  providers: [OfficeAutomationService, OfficeAutomationQueryService, OfficeAttachmentService],
+  exports: [OfficeAutomationService, OfficeAutomationQueryService, OfficeAttachmentService],
 })
 export class OfficeAutomationModule {}
